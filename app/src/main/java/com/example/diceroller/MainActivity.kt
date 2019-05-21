c518ea7531228c2be47bb33085c0e1b095023719
Ilija Tomic <ilija.tomic@merasws.rs>
2019-05-21 20:47:37 +0200

    update

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.diceroller
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,19 +16,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
 
         diceImage = findViewById(R.id.dice_image)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val aboutMe: Button = findViewById(R.id.about_me_button)
+        aboutMe.setOnClickListener { view -> onClick(view) }
+        rollButton.setOnClickListener { view -> onClick(view) }
+    }
+
+    private fun onClick(view: View) {
+        when (view.id) {
+            R.id.roll_button -> rollDice()
+            R.id.about_me_button -> startAboutMe()
+        }
+    }
+
+    private fun startAboutMe() {
+        val intent = Intent(this, AboutMeActivity::class.java)
+        startActivity(intent)
     }
 
     private fun rollDice() {
 
-        var randomInt = Random().nextInt(6) + 1
-
-        val drawableRes = when (randomInt) {
+        val drawableRes = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
